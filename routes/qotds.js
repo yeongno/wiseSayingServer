cc7d1cf67b4c98813372ba10cf1c6d180b76e5fe
lyn <dldudsh83@gmail.com>
2023-03-14 00:09:13 +0900

    quotes 인용 페이지1

--- routes/qotds.js
+++ routes/qotds.js
@@ -6,19 +6,16 @@ const https = require("https");
 const { default: quotesInstance } = require("../instance/quotesInstance");
 const api_key = "37017bba672eb45b94132c5e6f1af6e6";
 
-router.get("/quotes", async () => {
+router.get("/quotes", async (req, res) => {
   quotesInstance
-    .get("/api/quotes/?page=1", {
-      headers: {
-        Authorization: `Token ${api_key}`,
-      },
-    })
+    .get("/api/quotes/?page=1")
     .then((response) => {
       const quotes = response.data.quotes;
-      console.log(quotes);
+      res.send(quotes);
     })
     .catch((err) => {
       console.error(err);
+      res.send("Error fetching quotes.");
     });
 });
 
