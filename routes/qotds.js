d3e74e92e1e9ccca7d3d9eac6bb2551ea1efc421
lyn <dldudsh83@gmail.com>
2023-03-13 23:33:16 +0900

    check quotes list

--- routes/qotds.js
+++ routes/qotds.js
@@ -1,6 +1,37 @@
 const { default: axios } = require("axios");
 const express = require("express");
 const router = express.Router();
+const https = require("https");
+const api_key = "37017bba672eb45b94132c5e6f1af6e6";
+
+router.get("/quotes", (req, res) => {
+  const options = {
+    hostname: "favqs.com",
+    path: `/api/quotes/?page=1`,
+    headers: {
+      Authorization: `Token ${api_key}`,
+    },
+  };
+
+  https
+    .get(options, (response) => {
+      let data = "";
+
+      response.on("data", (chunk) => {
+        data += chunk;
+      });
+
+      response.on("end", () => {
+        const quotes = JSON.parse(data).quotes;
+
+        res.send(quotes);
+      });
+    })
+    .on("error", (err) => {
+      console.error(err);
+      res.status(500).send("Error occurred while fetching quotes");
+    });
+});
 
 router.get("/qotd", async (req, res) => {
   try {
