8a32aedb5871c384228f07dd089117a7175a441d
lyn <dldudsh83@gmail.com>
2023-03-15 00:03:14 +0900

    edit

--- routes/quotes/filter/filters.js
+++ routes/quotes/filter/filters.js
@@ -7,7 +7,7 @@ router.get("/quotes/filter/inspiration", async (req, res) => {
   quotesInstance
     //   https://favqs.com/api/quotes/?filter=funny
     // .get("/api//quotes/?page=1&filter=inspiration")
-    .get("/api//quotes/?filter=inspiration")
+    .get("/api/quotes/?filter=inspiration")
     .then((response) => {
       const quotes = response;
       res.send(quotes);
